c9485a716b9af8314c279569418f420bb48bf3e2
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-07-18 20:00:39 +0000

    change callback url

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,7 +3,7 @@
 module.exports = {
 	'twitterAuth': {
 		'consumerSecret': 'i0QOgQRv9fOJ2Wlns9nrAJTiNCJl0bHH4UELsA0g067vYfr8AO',
-		'callbackURL': 'https://votingapp-galaxyhitcher.c9users.io/auth/twitter/callback',
+		'callbackURL': 'http://powerful-tundra-47780.herokuapp.com//auth/twitter/callback',
 		'consumerKey': 'CbdQxtjnrUdir2Can4nCg7Knt',
 	}
 };
